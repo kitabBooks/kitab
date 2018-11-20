fa351b59b66c85d69c1ce38cf65d0923e2d0d3f5
Elaine Henrique <silva.elainehenrique@gmail.com>
2018-11-20 17:02:52 -0200

    excluding models publisher

--- startup/app.js
+++ startup/app.js
@@ -54,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', authRoutes);
 app.use('/', indexRouter);
+app.use('/', bookRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
