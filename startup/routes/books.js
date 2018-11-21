3d6bfd904d144606529e4e5f546171850da778f4
Gabriel Sicuto <gabrielsicuto@gmail.com>
2018-11-21 19:47:05 -0200

    ajust for API books use

--- startup/routes/books.js
+++ startup/routes/books.js
@@ -1,17 +1,27 @@
 const express = require('express');
-const Book = require ('../models/books');
+const axios = require('axios');
+const Book = require('../models/books');
 
+const CircularJSON = ('circular-json');
 const router = express.Router();
 
 /* GET books page. */
 router.get('/books', (req, res, next) => {
-  Book.find()
-    .then((books) => {
+  axios.get('https://www.googleapis.com/books/v1/volumes?q=javascript&key=AIzaSyBFY6x3Zudnvc2BdtAqpVsRk_lhfVahjnc')
+    .then((response) => {
+      console.log(response);
+      const books = response;
       res.render('bookgallery', { books });
-    })
-    .catch((error) => {
-      console.log(error);
+    }).catch((err) => {
+      console.log(err);
     });
+  // Book.find()
+  //   .then((books) => {
+  //     res.render('bookgallery', { books });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 });
 
 
