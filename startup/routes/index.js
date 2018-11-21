4607a92bfce14b00c0d40f8c5c7b7bf10a220324
Gabriel Sicuto <gabrielsicuto@gmail.com>
2018-11-21 13:09:29 -0200

    resolve nav bar layout with user

--- startup/routes/index.js
+++ startup/routes/index.js
@@ -4,7 +4,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Kitab' });
+  res.render('index', { title: 'Kitab', user: req.user });
 });
 //  router.get('/signup',function(req, res, next) {
 //   res.send('hkjkhlhk');
