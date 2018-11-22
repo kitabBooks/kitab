ec607b4cc8c6d9052ac7ede4978c35f82b7561a4
Elaine Henrique <silva.elainehenrique@gmail.com>
2018-11-22 17:09:12 -0200

    about route done

--- startup/routes/auth/dashboard.js
+++ startup/routes/auth/dashboard.js
@@ -4,8 +4,45 @@ const express = require('express');
 
 const authRoutes = express.Router();
 
-authRoutes.get('/dashboard', ensureLogin.ensureLoggedIn({ baseUrl: '/', redirectTo: '/users/signin' }), (req, res) => {
-  res.render('dashboard', { user: req.user });
+authRoutes.get('/dashboard', ensureLogin.ensureLoggedIn({
+  baseUrl: '/',
+  redirectTo: '/users/signin'
+}), (req, res) => {
+  res.render('dashboard', {
+    user: req.user
+  });
 });
+
+authRoutes.get('/interest', ensureLogin.ensureLoggedIn({
+  baseUrl: '/',
+  redirectTo: '/users/signin'
+}), (req, res, next) => {
+  res.render('interests');
+});
+authRoutes.post('/interests', (req, res, next) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const userId = req.user._id;
+  const userInfo = {
+    interest: ['books added'],
+  };
+  User.findByIdAndUpdate(userId, userInfo, {
+    new: true
+  }, (err, theUser) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    req.session.currentUser = theUser;
+
+    res.redirect('/dashboard');
+  });
+});
+
+
+
+
+
+
 
 module.exports = authRoutes;
